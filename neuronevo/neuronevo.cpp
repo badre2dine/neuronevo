bf94f5d72b0454703070fccc5d8ceff7b7770202
badredinne bencherki <badr-bencherki@live.fr>
2019-06-26 22:36:04 +0100

    add basic classse for calcul

--- neuronevo/neuronevo.cpp
+++ neuronevo/neuronevo.cpp
@@ -1,20 +1,31 @@
-// neuronevo.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+// neuroVolution.cpp : définit le point d'entrée pour l'application console.
 //
 
-#include <iostream>
+#include "stdafx.h"
+#include <ctime>
+#include <cmath>
+
+#include"NN.h"
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	NN brain;
+
+
+
+
+	Matrix<int> m(2,2,arrayAloc<int, 2, 2>(new int[2][2]{ {1,0} ,{7,5} }));
+
+	
+
+	brain.addLayer(2, SIGMOID); 
+	brain.addLayer(10, SOFTAMAX);
+	brain.initial(RANDOM); 
+	brain.decision(new double[2]{ 0.2,0.3 }).transpose().show();
+	cout << m.getRow_size();
+	int c;
+	cin >> c;
+	return 0;
 }
 
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
